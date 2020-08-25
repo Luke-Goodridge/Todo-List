e0f23f466a5b42838143c3e8548904e973bc741a
DESKTOP-R53PPF3\luke_ <lukegoodridge@icloud.com>
2020-08-25 22:29:02 +0100

    Updates

---
+++ precache-manifest.9cb6325133a045aacf57bcdece89caf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e9dc93839c29da60bbe7f6f305258d",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "7fe11d04bb7ff4442b07",
+    "url": "/Todo-List/static/css/main.42f820f7.chunk.css"
+  },
+  {
+    "revision": "1a0280f0cb2c9a17bd4e",
+    "url": "/Todo-List/static/js/2.8640f28e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-List/static/js/2.8640f28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe11d04bb7ff4442b07",
+    "url": "/Todo-List/static/js/main.641adcf8.chunk.js"
+  },
+  {
+    "revision": "e6a73e60887bb7c0c63a",
+    "url": "/Todo-List/static/js/runtime-main.a3320050.js"
+  }
+]);
